6e5b019c6aef2dd070fa709900935ff65495a898
Doug Keefe <doug.keefe@gmail.com>
2023-08-15 23:56:08 -0300

    Added LICENSE file and updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,112 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 font-inter">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4">Public Servant Salary Data API</h1>
+        <p className="mb-6">
+          This API provides access to public servant salary data for various classifications and levels.
+          The information is sourced from{' '}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://www.tbs-sct.canada.ca/pubs_pol/hrpubs/coll_agre/rates-taux-eng.asp"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-blue-500 underline"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            TBS-SCT Canada
+          </a>{' '}
+          and is available through a set of endpoints. More classifications will be added to the API soon.
+        </p>
+
+        <section className="mb-8">
+          <h2 className="text-xl font-semibold mb-2">Endpoints</h2>
+          <ul className="list-disc pl-6">
+            <li>
+              <code>/api/data</code>: Returns the full JSON containing salary data for all classifications and levels.
+            </li>
+            <li>
+              <code>/api/top</code>: Returns the top salary level for each classification and level.
+            </li>
+            <li>
+              <code>/api/[classification-level]</code>: Returns the full salary information for a specific classification
+              and level (e.g., <code>/api/cs-01</code>).
+            </li>
+            <li>
+              <code>/api/[classification-level]/[step-#]</code>: Returns the salary for the specified step of a
+              classification and level (e.g., <code>/api/cr-1/step-3</code>).
+            </li>
+            <li>
+              <code>/api/[classification-level]/top</code>: Returns the top salary amount of the specified classification
+              and level (e.g., <code>/api/as-4/top</code>).
+            </li>
+          </ul>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl font-semibold mb-2">Usage</h2>
+          <p>
+            To use the API, send HTTP requests to the specified endpoints using your preferred method (e.g., cURL, Python
+            requests, etc.).
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h3 className="text-lg font-semibold mb-2">Examples</h3>
+          <p className="mb-4">
+            Here are some example HTTP requests you can use to interact with the API:
+          </p>
+          <div className="rounded-md p-4">
+            <pre>
+              <code>
+{`  # Retrieve full salary data
+  curl https://your-api-url/api/data
+
+  # Get top salary levels
+  curl https://your-api-url/api/top
+
+  # Get full salary info for a classification and level
+  curl https://your-api-url/api/cs-01
+
+  # Get salary for a specific step
+  curl https://your-api-url/api/cr-1/step-3
+
+  # Get top salary amount for a classification and level
+  curl https://your-api-url/api/as-4/top
+`}
+              </code>
+            </pre>
+          </div>
+        </section>
+
+
+
+        <section className="mb-8">
+          <h2 className="text-xl font-semibold mb-2">Contributing</h2>
+          <p>
+            Contributions are welcome! If you have any suggestions, enhancements, or bug fixes, please submit an issue or
+            a pull request on the <a href="https://github.com/dougkeefe/ps-salary-data" className="text-blue-500">GitHub repository</a>
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl font-semibold mb-2">License</h2>
+          <p>
+            This project is licensed under the{' '}
+            <a href="https://github.com/dougkeefe/ps-salary-data/blob/main/LICENSE" className="text-blue-500">
+              MIT License
+            </a>
+            .
+          </p>
+        </section>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="max-w-3xl mx-auto text-gray-500">
+        <hr className="mb-4" />
+        <p>    This API and website are not affiliated with, endorsed, or sponsored by the Government of Canada. <br />
+          The information provided is sourced from publicly available data and is provided for educational and informational purposes only.
+        </p>
+      </footer>
     </main>
-  )
+  );
 }
