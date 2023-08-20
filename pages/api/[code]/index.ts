187b8f6d7c1ab16a9c1f5d3ed787438a9ca1f3c7
Doug Keefe <doug.keefe@gmail.com>
2023-08-19 21:32:21 -0300

    Corrected the Classification summary endpoint

--- pages/api/[code]/index.ts
+++ pages/api/[code]/index.ts
@@ -2,6 +2,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs/promises';
 import path from 'path';
 
+type JsonData = {
+  [key: string]: any;
+};
+
+function findAllInKeys(data: JsonData, searchStr: string): JsonData {
+  let matches: JsonData = {};
+  for (let key in data) {
+    if (key.includes(searchStr)) {
+      matches[key] = data[key];
+    }
+  }
+  return matches;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // Define the path to the JSON file
@@ -25,17 +39,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(400).json({ error: 'Failed to parse the JSON file' });
     }
 
+    const validData = jsonData as JsonData;
+
     // Extract the desired subset based on the dynamic segment (code)
     const desiredSubset = req.query.code?.toString().toUpperCase();
 
-    if (desiredSubset && jsonData[desiredSubset]) {
-      res.status(200).json(jsonData[desiredSubset]);
+    if (!desiredSubset) {
+      return res.status(400).json({ error: 'Please provide a subset to search for.' });
+    }
+
+    // Search based on a string in the key
+    const results = findAllInKeys(validData, desiredSubset);
+
+    if (Object.keys(results).length > 0) {
+      res.status(200).json(results);
     } else {
-      res.status(404).json({ error: 'Subset not found' });
+      res.status(404).json({ error: 'Subset ' + desiredSubset + ' not found in any keys' });
     }
 
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An unexpected error occurred' });
   }
+
 }
+
